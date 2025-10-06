32d0e2912ce3eee34b06133c1958743b35c2dbc6
yusuke.tsuchiya <yusuke.tsuchiya@databricks.com>
2025-10-07 08:26:59 +0900

    modify link

--- e2e_asset/05_アプリ起動.py
+++ e2e_asset/05_アプリ起動.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC [e2e_visual_inspection/visual_inspection_app/README.md](e2e_visual_inspection/visual_inspection_app/README.md)を参照
+# MAGIC [visual_inspection_app/README.md](../visual_inspection_app/README.md)を参照
 
 # COMMAND ----------
 
